65f34ee8851fc87a3a6507665df034a0e60c4772
BetaCat_HA <wangjianyu2311077@gmail.com>
2023-06-07 08:34:29 +0800

    添加：测试类

---
+++ spring-boot/src/test/java/cn/com/betacat/TestUserController.java
@@ -0,0 +1,23 @@
+package cn.com.betacat;
+
+import cn.com.betacat.dao.UserMapper;
+import cn.com.betacat.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class TestUserController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void testGetUser() {
+        List<User> userList = userMapper.selectList(null);
+        for (User user : userList) {
+            System.out.println("user = " + user);
+        }
+    }
+}
