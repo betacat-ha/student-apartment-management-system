38b8a6400d8ecbff37694ec25761b421d92b0ae9
BetaCat_HA <wangjianyu2311077@gmail.com>
2023-06-15 10:11:03 +0800

    修改：移除MyBatis-Plus-Joined的依赖

--- spring-boot/src/main/java/cn/com/betacat/dao/UsageMapper.java
+++ spring-boot/src/main/java/cn/com/betacat/dao/UsageMapper.java
@@ -2,13 +2,13 @@ package cn.com.betacat.dao;
 
 import cn.com.betacat.entity.Apartment;
 import cn.com.betacat.entity.Usage;
-import com.github.yulichang.base.MPJBaseMapper;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.*;
 
 import java.util.List;
 
 @Mapper
-public interface UsageMapper extends MPJBaseMapper<Usage> {
+public interface UsageMapper extends BaseMapper<Usage> {
     @Deprecated
     @Select("select * from usage")
     @Results({
