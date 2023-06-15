74dc656d652873f6104e2d9543cae3c5c13b6363
BetaCat_HA <wangjianyu2311077@gmail.com>
2023-06-15 16:06:57 +0800

    添加：楼栋管理

--- spring-boot/src/main/java/cn/com/betacat/controller/BuildingController.java
+++ spring-boot/src/main/java/cn/com/betacat/controller/BuildingController.java
@@ -1,13 +1,16 @@
 package cn.com.betacat.controller;
 
+import cn.com.betacat.dao.ApartmentMapper;
 import cn.com.betacat.dao.BuildingMapper;
+import cn.com.betacat.entity.Apartment;
 import cn.com.betacat.entity.Building;
 import cn.com.betacat.entity.Result;
+import cn.com.betacat.entity.Usage;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin
@@ -16,10 +19,76 @@ public class BuildingController {
     @Autowired
     private BuildingMapper buildingMapper;
 
+    @Autowired
+    private ApartmentMapper apartmentMapper;
+
 
     @GetMapping("/api/building")
     public Result query() {
         List<Building> list = buildingMapper.selectAllBuildingsAndApartments();
         return new Result(200, "OK", list);
+    }
+
+    @PostMapping("/api/building")
+    public Result update(@RequestBody Building building) {
+        //buildingMapper.insert(building);
+        boolean haveError = false;
+
+        if (building.getApartments() == null ||
+                building.getName() == null || building.getName().equals("")
+        ) {
+            return new Result(400, "请检查填写的数据", null);
+        }
+
+        if (building.getId() == null || building.getId().equals(0)) {
+            buildingMapper.insert(building);
+        } else {
+            buildingMapper.updateById(building);
+        }
+
+        // 分别更新每个房间数据
+        for (Apartment apartment : building.getApartments()) {
+            if (apartment.getName() == null || apartment.getName().equals("")) {
+                haveError = true;
+                break;
+            }
+            apartment.setBuildingId(building.getId());
+
+            if (apartment.getId() == null || apartment.getId().equals(0)) {
+                apartmentMapper.insert(apartment);
+            } else {
+                apartmentMapper.updateById(apartment);
+            }
+        }
+
+        // MARK: 删除多余的房间模块
+
+        // 获取数据库中已有的房间
+        QueryWrapper<Apartment> wrapper = new QueryWrapper<>();
+        wrapper.eq("building_id", building.getId());
+        List<Apartment> existingApartments = apartmentMapper.selectList(wrapper);
+
+        // 获取所有房间的id
+        List<Integer> apartmentIds = new ArrayList<>();
+        for (Apartment apartment : building.getApartments()) {
+            apartmentIds.add(apartment.getId());
+        }
+
+        // 删除多余的房间
+        for (Apartment existingApartment : existingApartments) {
+            if (!apartmentIds.contains(existingApartment.getId())) {
+                apartmentMapper.deleteById(existingApartment.getId());
+            }
+        }
+
+        return haveError ?
+                new Result(400, "部分数据未保存，请仔细检查！", null) :
+                new Result(200, "OK", null);
+    }
+
+    @DeleteMapping("/api/building")
+    public Result delete(Integer id) {
+        buildingMapper.deleteById(id);
+        return new Result(200, "OK", null);
     }
 }
