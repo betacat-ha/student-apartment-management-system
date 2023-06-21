00a2506176f185cf363d92848bab6afe7c6a1aa7
BetaCat_HA <wangjianyu2311077@gmail.com>
2023-06-21 08:08:07 +0800

    添加：excel处理

--- spring-boot/src/main/java/cn/com/betacat/entity/Student.java
+++ spring-boot/src/main/java/cn/com/betacat/entity/Student.java
@@ -1,5 +1,6 @@
 package cn.com.betacat.entity;
 
+import com.alibaba.excel.annotation.*;
 import com.baomidou.mybatisplus.annotation.TableField;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
@@ -7,16 +8,36 @@ import lombok.Data;
 
 @Data
 public class Student {
+    @ExcelProperty("学号")
     private String id;
-    private Integer apartmentId;
+
     @TableField(exist = false)
     @JsonIgnore
+    @ExcelIgnore
     private Apartment apartment;
+
+    @TableField(exist = false)
+    @ExcelProperty("楼栋号")
+    private String buildingName;
+
+    @TableField(exist = false)
+    @ExcelProperty("宿舍号")
+    private String apartmentName;
+
+    @ExcelProperty("宿舍id")
+    private Integer apartmentId;
+    @ExcelProperty("班级")
     private String className;
+    @ExcelProperty("姓名")
     private String name;
+    @ExcelProperty("年龄")
     private Integer age;
+    @ExcelProperty("性别")
     private String gender;
+    @ExcelProperty("电话")
     private String phone;
+    @ExcelProperty("邮箱")
     private String email;
+    @ExcelProperty("状态")
     private String status;
 }
---
+++ spring-boot/src/main/java/cn/com/betacat/services/ResponseExcel.java
@@ -0,0 +1,23 @@
+package cn.com.betacat.services;
+
+import com.alibaba.excel.converters.Converter;
+import com.alibaba.excel.support.ExcelTypeEnum;
+import com.alibaba.excel.write.handler.WriteHandler;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ResponseExcel {
+    String name() default "";
+    ExcelTypeEnum suffix() default ExcelTypeEnum.XLSX;
+    String password() default "";
+    String[] sheet() default {};
+    boolean inMemory() default false;
+    String template() default "";
+    String[] include() default {};
+    String[] exclude() default {};
+    Class<? extends WriteHandler>[] writeHandler() default {};
+    Class<? extends Converter>[] converter() default {};
+}
