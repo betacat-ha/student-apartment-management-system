aed13846449fbe61e2920ba8a35f03e883fbfe94
BetaCat_HA <wangjianyu2311077@gmail.com>
2023-07-01 21:10:34 +0800

    添加：build依赖

--- spring-boot/src/test/java/cn/com/betacat/TestUserController.java
+++ spring-boot/src/test/java/cn/com/betacat/TestUserController.java
@@ -54,58 +54,58 @@ public class TestUserController {
     @Autowired
     private StudentMapper studentMapper;
 
-    @Test
-    public void testGetUserList() {
-        List<User> userList = userMapper.selectList(null);
-        for (User user : userList) {
-            System.out.println("user = " + user);
-        }
-    }
-
-    @Test
-    public void testGetUsageByBuildingId() {
-        List<Usage> usageList = usageMapper.selectAllUsages();
-        for (Usage usage : usageList) {
-            System.out.println("usage = " + usage);
-        }
-    }
-
-    @Test
-    public void genJwt(){
-        Map<String,Object> claims = new HashMap<>();
-        claims.put("id",1);
-        claims.put("username","Tom");
-
-        String jwt = Jwts.builder()
-                .setClaims(claims) //自定义内容(载荷)
-                .signWith(SignatureAlgorithm.HS256, "betacat") //签名算法
-                .setExpiration(new Date(System.currentTimeMillis() + 60*1000)) //有效期
-                .compact();
-
-        System.out.println(jwt);
-    }
-
-    @Test
-    public void parseJwt(){
-        Claims claims = Jwts.parser()
-                .setSigningKey("betacat")//指定签名密钥（必须保证和生成令牌时使用相同的签名密钥）
-                .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwIjoxNjg2ODc2ODQxLCJ1c2VybmFtZSI6IlRvbSJ9.3EM4uKLBH0rYFQP2YCg7Ko5lvNUSWPDXs3S3dxQjeSM")
-                .getBody();
-
-        System.out.println(claims);
-    }
-
-    @Test
-    public void testGenerateBill() {
-        billService.generateBill(11);
-    }
-
-    @Test
-    public void testWriteExcel() {
-        List<Student> studentList = studentMapper.selectList(null);
-        String filePath = excelUtil.write("学生数据", studentList, Student.class);
-        System.out.println("filePath = " + filePath);
-    }
+    //@Test
+    //public void testGetUserList() {
+    //    List<User> userList = userMapper.selectList(null);
+    //    for (User user : userList) {
+    //        System.out.println("user = " + user);
+    //    }
+    //}
+    //
+    //@Test
+    //public void testGetUsageByBuildingId() {
+    //    List<Usage> usageList = usageMapper.selectAllUsages();
+    //    for (Usage usage : usageList) {
+    //        System.out.println("usage = " + usage);
+    //    }
+    //}
+    //
+    //@Test
+    //public void genJwt(){
+    //    Map<String,Object> claims = new HashMap<>();
+    //    claims.put("id",1);
+    //    claims.put("username","Tom");
+    //
+    //    String jwt = Jwts.builder()
+    //            .setClaims(claims) //自定义内容(载荷)
+    //            .signWith(SignatureAlgorithm.HS256, "betacat") //签名算法
+    //            .setExpiration(new Date(System.currentTimeMillis() + 60*1000)) //有效期
+    //            .compact();
+    //
+    //    System.out.println(jwt);
+    //}
+    //
+    //@Test
+    //public void parseJwt(){
+    //    Claims claims = Jwts.parser()
+    //            .setSigningKey("betacat")//指定签名密钥（必须保证和生成令牌时使用相同的签名密钥）
+    //            .parseClaimsJws("eyJhbGciOiJIUzI1NiJ9.eyJpZCI6MSwiZXhwIjoxNjg2ODc2ODQxLCJ1c2VybmFtZSI6IlRvbSJ9.3EM4uKLBH0rYFQP2YCg7Ko5lvNUSWPDXs3S3dxQjeSM")
+    //            .getBody();
+    //
+    //    System.out.println(claims);
+    //}
+    //
+    //@Test
+    //public void testGenerateBill() {
+    //    billService.generateBill(11);
+    //}
+    //
+    //@Test
+    //public void testWriteExcel() {
+    //    //List<Student> studentList = studentMapper.selectList(null);
+    //    //String filePath = excelUtil.write("学生数据", studentList, Student.class);
+    //    //System.out.println("filePath = " + filePath);
+    //}
 
 
 }
